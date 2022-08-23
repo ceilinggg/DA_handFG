23b5a100f697e91daba2b8674240ccd5a8284751
Celine <line.blublue@gmail.com>
2022-08-23 14:45:20 +0800

    update

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ print("**********")
 for x in h.headers:
     print("\t ", x, ":", h.headers[x])
 print("**********")
+# This will modify the headers user-agent
+headers = {
+    'User-Agent' : 'Mobile'
+}
+# Test on external site
+url2 = 'http://172.18.58.80/headers.php'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
