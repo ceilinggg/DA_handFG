9e5debda08e4d6b6770cdac069562d15f0532dea
Celine <line.blublue@gmail.com>
2022-08-23 15:14:33 +0800

    update

---
+++ scrapytest.py
@@ -0,0 +1,11 @@
+import scrapy
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['http://172.18.58.80/index.php']
+    def parse(self, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield {
+                'Image Link': x.xpath(newsel).extract_first(),
+            }
