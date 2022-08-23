a39100ae090268c6d72678ab1f93c46a1d7d0a6c
Celine <line.blublue@gmail.com>
2022-08-23 14:36:39 +0800

    update

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import requests
 url = 'http://172.18.58.80/index.php'
 r = requests.get(url)
 print(r.text)
+# This will get the status code
+print("Status code:")
+print("\t *", r.status_code)
+# This will get just the headers
+h = requests.head(url)
+print("Header:")
+print("**********")
+# To print line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
